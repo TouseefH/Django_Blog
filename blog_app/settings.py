2628ba9a0000449ba355595e139e9931ed9038cd
Touseef Hanif <touseefhanif001@gmail.com>
2024-07-23 21:37:25 +0500

    Adding Meida Files

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,7 +122,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
+
+MEDIA_URL = '/media/'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- users/views.py
+++ users/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+
+from django.contrib.auth import logout
+
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -9,8 +13,20 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
+
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'users/logout.html')
+
+
